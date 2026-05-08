e99fdb4547e65dba8273af099163b4f48c292ec1
onnwannarat <onnwannarat@gmail.com>
2026-05-08 15:04:38 +1000

    created macOS version

--- dist/XplorValidator/_internal/pandas/__init__.py
+++ dist/XplorValidator/_internal/pandas/__init__.py
@@ -1,17 +1,5 @@
 from __future__ import annotations
 
-
-# start delvewheel patch
-def _delvewheel_patch_1_12_0():
-    import os
-    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'pandas.libs'))):
-        os.add_dll_directory(libs_dir)
-
-
-_delvewheel_patch_1_12_0()
-del _delvewheel_patch_1_12_0
-# end delvewheel patch
-
 __docformat__ = "restructuredtext"
 
 # Let users know if they're missing any of our hard dependencies
